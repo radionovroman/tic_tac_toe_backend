3df91b4c78c6b9e34af864b35aa3c8f9cae6d930
radionovroman <89083056+radionovroman@users.noreply.github.com>
2024-09-29 18:54:22 +0700

    Modified urls.py

--- tic_tac_toe_images/tic_tac_toe_images/urls.py
+++ tic_tac_toe_images/tic_tac_toe_images/urls.py
@@ -19,11 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('images.api_urls')),
     path('api-token-auth/', obtain_auth_token),  # Endpoint to obtain auth token
+    path('', RedirectView.as_view(url='/api/')),
 ]
 
 if settings.DEBUG:
