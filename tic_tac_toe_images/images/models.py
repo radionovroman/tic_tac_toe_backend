702a58c9b50ad871dd066f0d56e5679eb71f19ee
radionovroman <89083056+radionovroman@users.noreply.github.com>
2024-10-16 19:30:31 +0700

    Add CloudinaryField to Image model

--- tic_tac_toe_images/images/models.py
+++ tic_tac_toe_images/images/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField  # Import CloudinaryField
 
 class SharedGame(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -14,7 +15,7 @@ class Image(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     shared_game = models.ForeignKey(SharedGame, on_delete=models.SET_NULL, null=True, blank=True)
     filename = models.CharField(max_length=100)
-    file = models.ImageField(upload_to='uploads/')
+    file = CloudinaryField('image')  # Change ImageField to CloudinaryField
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
